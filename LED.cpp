1a09e0b35508db9837f1f01e8abb2a5dec27305e
mRaines08 <mallory.raines@stu.daviess.kyschools.us>
2026-09-11 19:19:11 -0500

    Test of LED file commit

---
+++ LED.cpp
@@ -0,0 +1,5 @@
+//must include arduino and the header file
+#include "Arduino.h";
+#include "LED.h";
+
+//i am adding a change
---
+++ LED.h
@@ -0,0 +1,21 @@
+//checks to see if there is already an instance of this file
+//if there is stop, dont run, it will break
+#ifndef LED_h;
+#define LED_h;
+
+//includes the arduino functions
+#include "Ardunio.h";
+
+
+class LED{
+
+    //must define scope
+    public:
+
+
+    private:
+
+    
+}
+
+#endif
