60339ce0bb723643858f258992ae8f2e0951294e
mRaines08 <mallory.raines@stu.daviess.kyschools.us>
2026-09-14 21:50:56 -0500

    untested completed file

--- LED.cpp
+++ LED.cpp
@@ -2,4 +2,45 @@
 #include "Arduino.h";
 #include "LED.h";
 
-//i am adding a change
+//constructor
+LED::LED(int p){
+    bool state = false;
+    int pinNum = p;
+
+    pinMode(pinNum, OUTPUT)     //sets the pin the LED is on to be an output, or controllable
+}
+
+bool LED::toggle(){
+    if !state{
+        on();
+        return true;
+    }
+    else{
+        off();
+        return false;
+    }
+}
+
+void on(){
+    digitalWrite(pinNum, HIGH);
+}
+
+void off(){
+    digitalWrite(pinNum, LOW);
+}        
+
+void flashRetrieval(){
+    on();
+    delay(1000);
+    off();
+    delay(1000);
+}
+
+bool getState(){
+    if state{
+        return true;
+    }
+    else{
+        return false;
+    }
+}  
--- LED.h
+++ LED.h
@@ -12,9 +12,18 @@ class LED{
     //must define scope
     public:
 
+    //defualt contructor for LED pin, takes 1 int for the pin number
+    LED(int p);
+
+    //meathods
+    bool toggle();        //will toggle LED and return the state of the LED
+    void on();            //will turn on LED
+    void off();           //will turn off LED
+    void flashRetrieval() //will flash lights at regular intervals for retrieval
+    bool getState();      //will return the state of the LED
 
     private:
-
+    bool state;
     
 }
 
