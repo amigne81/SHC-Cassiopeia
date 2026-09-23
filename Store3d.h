573090b39f4e1e67ad4bffb3f4d32d9a9b882148
amigne81 <apg0013@uah.edu>
2026-09-23 17:34:20 -0500

    Define store3D class with coordinate management
    Added class store3D with private coordinates and public delta variables.

--- Store3d.h
+++ Store3d.h
@@ -1,9 +1,18 @@
 
+#ifndef store3D_h
+#define store3D_h
 
-
-
-
-
-
-
-
+class store3D{
+	private:
+	int x,y,z;
+	public:
+	int dx=0 ,dy=0, dz=0; // change in direction variables
+	// sets value for private values and updates their respective deltas
+	void setX(int a);
+	void setY(int a);
+	void setZ(int a);
+	// outputs values in csv format
+	string outCSV();
+	string outDCSV();
+}
+#endif
